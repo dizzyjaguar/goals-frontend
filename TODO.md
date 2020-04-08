156a7bfa30bf2b41381912c221e5b2d5be9fe2b1
Eli Zevin <dizzyjaguar@gmail.com>
2020-04-07 20:29:11 -0700

    make the request to the database again after handleDelete, need to make it happen with state, so it looks like it happens instatnly while the request happens in the background

--- TODO.md
+++ TODO.md
@@ -2,7 +2,11 @@
 
 2. go through official react tutorial
 
-3. create delete button for each todo item
+DONE---3. create delete button for each todo item 
 
 4. create update button for each todo item
 
+---NEXT---
+5. consider changing state first so it looks like it loads really quick, while
+actually the request call is happening in the background, but state causes it to 
+look like it changed instantly
--- src/TodoList.js
+++ src/TodoList.js
@@ -11,6 +11,7 @@ export default class TodoList extends Component {
     
   }
 
+  
   componentDidMount = async() => {
     const data = await getTodoList();
     console.log(data.body);
@@ -23,7 +24,11 @@ export default class TodoList extends Component {
   handleDelete = async (todo) => {
     await request.delete(`http://localhost:7890/api/v1/todos/${todo._id}`)
     console.log('this delete function is firing')
-
+    //below me needs to get turned into its own function, instead of calling it twice, just like on componentDidMount
+    const data = await getTodoList();
+    this.setState({
+      todos: data.body
+    })
   }
 
   render() {
@@ -42,6 +47,7 @@ export default class TodoList extends Component {
           this.state.todos.map(todo => <TodoItem 
           todo={ todo }          
           handleDelete={this.handleDelete}
+          key={ todo._id }
 
            /> )
         }
