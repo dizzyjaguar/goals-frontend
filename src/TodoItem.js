8e0e47298cf61faf0c34de71847763ae2daeecc1
Eli Zevin <dizzyjaguar@gmail.com>
2020-04-08 09:41:13 -0700

    minor css fix

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -10,7 +10,7 @@ export default class TodoItem extends Component {
       <div className='TodoItem'>
         <h3 className='Todo-title'> {todo.title} </h3>
         <p className='Todo-desc'> {todo.description} </p>
-        <button onClick={() => this.props.handleDelete(this.props.todo)}>Delete</button>
+        <button id='Todo-delete' onClick={() => this.props.handleDelete(this.props.todo)}>Delete</button>
       </div>
     )
   }
