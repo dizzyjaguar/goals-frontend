20380699e41a51924e17f7207cf751039d5d3111
Eli Zevin <dizzyjaguar@gmail.com>
2020-04-09 19:03:23 -0700

    successfully rendering todo title as a link when its in a list component and NOT as a link when in detail mode

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,14 +3,20 @@ import { Link } from 'react-router-dom';
 
 
 export default class TodoItem extends Component {
-  
 
-  render() {
+  render() { 
+       
     const { todo } = this.props;
     return (
       <div className='TodoItem'>
-        <Link to={`todo/${todo._id}`}> <h3 className='Todo-title'> {todo.title} </h3>
-        </Link>
+        
+        {
+          window.location.href.includes('todos') 
+          ? <Link to={`todo/${todo._id}`}> <h3 className='Todo-title'> {todo.title} </h3> </Link>
+          
+          : <h3 className='Todo-title'> {todo.title} </h3>
+        }
+
         <p className='Todo-desc'> {todo.description} </p>
         <button id='Todo-update' onClick={() => this.props.handleUpdate(this.props.todo)}>Update</button>
         <button id='Todo-delete' onClick={() => this.props.handleDelete(this.props.todo)}>Delete</button>
