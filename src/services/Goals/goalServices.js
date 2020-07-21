f4f5ae76ec16be312bf3675c5a5177a48f943dfb
Eli Zevin <dizzyjaguar@gmail.com>
2020-07-21 09:42:40 -0700

    create getAllGoals service and leave notes for needed routes

--- src/services/Goals/goalServices.js
+++ src/services/Goals/goalServices.js
@@ -5,3 +5,9 @@ export const postGoalRequest = (goal) => {
     .then(res => res.body)
 };
 
+export const getAllGoalsRequest = () => {
+  return request.get('http://localhost:7890/api/v1/goals')
+    .then(res => res.body)
+};
+
+// i need to create something on the backed that will become a route that will grab all the goals created by the user, one for all the goals the user is working on, a route for all the goals a user has completed, etc.
