fdc9fc5b015c6601c0c242502e0f7d1d9fe9cc47
Eli Zevin <dizzyjaguar@gmail.com>
2020-07-23 09:16:21 -0700

    global goal detail working, with title as link

--- src/components/Goals/Goal.js
+++ src/components/Goals/Goal.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Goal = ({ goal }) => {
   return (
     <div className='Goal'>
-      <h2>{goal.title}</h2>
+      <Link to={`/global/${goal._id}`}> 
+      <h2>{goal.title}</h2> 
+      </Link>
       <h3>{goal.stars}</h3>
       <p>{goal.description}</p>
       <p>{goal.createdBy}</p>
