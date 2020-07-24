f80bb695bf5ff00564b35613ba2d212fa7e873f3
Eli Zevin <dizzyjaguar@gmail.com>
2020-07-24 10:09:21 -0700

    create createGoal form

---
+++ src/components/Forms/CreateGoal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useGoalForm } from '../../hooks/goalhooks/goalFormHooks';
+
+const CreateGoal = () => {
+  const { values, handlesChange, handleSubmit, user } = useGoalForm;
+  
+  return (
+    <>
+      <form>
+        <label>
+          Title:
+          <input name='title' type='text' value ={values.title} onChange={handlesChange} placeholder='title' />
+        </label>
+        <label>
+          Description:
+          <input name='description' type='text' value={values.description} onChange={handlesChange} placeholder='description' />
+        </label>
+        {
+          user ? <button type='submit' onClick={handleSubmit}>Create</button> : <p>Please Login First</p>
+        }
+        
+      </form>
+    </>
+  )
+}
+
+export default CreateGoal;
