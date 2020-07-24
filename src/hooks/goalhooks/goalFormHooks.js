31a1b08a112166ca6665ff988feb27eb28feef1e
Eli Zevin <dizzyjaguar@gmail.com>
2020-07-24 10:02:12 -0700

    create goalFormHooks

---
+++ src/hooks/goalhooks/goalFormHooks.js
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
+import { getUser } from '../../selectors/authSelector'
+import { createGlobalGoalRedux } from '../../actions/goalActions'
+
+export const useGoalForm = () => {
+  const dispatch = useDispatch()
+  const user = useSelector(getUser)
+  const [values, setValues] = useState({
+    createdBy: user._id,
+    title: '',
+    description: ''
+  });
+
+  const handleChange = (event) => {
+    const { target } = event;
+    const { name, value } = target;
+    event.persist();
+
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = async(event) => {
+    event.preventDefault()
+
+    dispatch(createGlobalGoalRedux(values))
+  };
+
+  return {
+    values,
+    handleChange,
+    handleSubmit,
+    user
+  }
+
+}
