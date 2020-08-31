f4bb32585542cee1410b4df3160d2fe6906d054f
Eli Zevin <dizzyjaguar@gmail.com>
2020-08-30 19:18:08 -0700

    start of creating Insight component, going to need to setup hooks for it and deal with only listening a few at a time

---
+++ src/components/Insights/Insight.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Insight = ({ insight }) => {
+  return (
+    <>
+      <div className='Insights'>
+        <p>hi this is a comment</p>
+        <p>by: {insight}</p>
+      </div>
+    </>
+  )
+}
+
+export default Insight;
